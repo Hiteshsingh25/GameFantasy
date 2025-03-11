800b92976fa2f682fd37459ede8098a620d3920f
Hiteshsingh25 <shekhawathitesh7@gmail.com>
2025-03-11 22:56:53 +0530

    to resolve build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,22 +5,24 @@ import UpcomingMatches from '../component/betMatched/UpcomingMatches'
 
 function Home() {
   return (
-    <DefaultView>
-      <div className='bg-slate-900 text-white'>
-     <img
-     src={img4}
-     className='h-52 w-full object-cover aspect-square rounded-sm '
-     />
-      </div>
-      <div>
-        <small className='text-yellow-500 font-bold text-[15px] px-2'>Live Score</small>
-        <LiveCard/>
-      </div>
-      <div> 
-        <small className='text-yellow-500 font-bold text-[15px] px-2'>Upcoming Score<span className='text-[11px] mx-2'>( Join contest to win more )</span></small>
-        <UpcomingMatches/>
-      </div>
-    </DefaultView>
+    // <DefaultView>
+    //   <div className='bg-slate-900 text-white'>
+    //  <img
+    //  src={img4}
+    //  className='h-52 w-full object-cover aspect-square rounded-sm '
+    //  />
+    //   </div>
+    //   <div>
+    //     <small className='text-yellow-500 font-bold text-[15px] px-2'>Live Score</small>
+    //     <LiveCard/>
+    //   </div>
+    //   <div> 
+    //     <small className='text-yellow-500 font-bold text-[15px] px-2'>Upcoming Score<span className='text-[11px] mx-2'>( Join contest to win more )</span></small>
+    //     <UpcomingMatches/>
+    //   </div>
+    // </DefaultView>
+    <>
+    </>
   )
 }
 
